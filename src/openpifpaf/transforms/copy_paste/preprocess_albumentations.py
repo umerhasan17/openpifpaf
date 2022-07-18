269ff4faddc0fb39328cc1364244bc6b9556940c
umerhasan17 <umerhasan17@gmail.com>
2022-07-18 12:09:11 +0200

    Update percentage

--- src/openpifpaf/transforms/copy_paste/preprocess_albumentations.py
+++ src/openpifpaf/transforms/copy_paste/preprocess_albumentations.py
@@ -100,7 +100,7 @@ class AlbumentationsComposeWrapper(Preprocess, A.Compose, metaclass=Albumentatio
                                   copy_paste_image_data['bboxes']],
                 )
                 cp_transform = A.Compose(
-                    [openpifpaf.transforms.CopyPaste(blend=True, sigma=1, pct_objects_paste=0.8, p=1)],
+                    [openpifpaf.transforms.CopyPaste(blend=True, sigma=1, pct_objects_paste=0.3, p=1)],
                     bbox_params=A.BboxParams(format="coco")
                 )
                 cp_output_data = cp_transform(**cp_data)
